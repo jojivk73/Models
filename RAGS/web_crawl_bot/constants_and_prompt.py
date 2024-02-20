337525953a3c19925d9f3b0d2439c7f25dfb151f
Jojimon Varghese <jojimon.varghese@intel.com>
2024-02-20 14:46:31 -0800

    Improved prompt

--- RAGS/web_crawl_bot/constants_and_prompt.py
+++ RAGS/web_crawl_bot/constants_and_prompt.py
@@ -20,24 +20,26 @@ single_link =True
 #search_str ="Intel CPU performance"
 search_str='https://www.pcmag.com/news/meteor-lake-first-tests-intel-core-ultra-7-benched-for-cpu-graphics-and'
 default_link =search_str
-#question ='Generate a table of processor performance comparisons that are in the doc'
+#question ='Generate a table of laptop performance comparisons that are in the doc'
 
 instr_prompt= 'Do the following instructions step by step.' + \
-'Step 1. List the processors in the document.' + \
-'Step 2. For each processor compare its performance with other processors in the document\n' + \
-'Step 3. Get the performance number for each processor:\n' + \
+'Step 1. List the laptops/CPU mentioned in the document.' + \
+'Step 2. Get the performance number for each laptop/CPU:\n' + \
 ' Example : Cinebench R23 - Multi-core CPU Score   H   14476\n' + \
-'Step 4. Generate a table as below for processors and their performance numbers\n' + \
+'Step 3. For each laptop/CPU compare its performance with other laptops/CPUs in the document\n' + \
+'Step 4. Generate a table as below for laptop/CPU and their performance numbers\n' + \
 ' Example table : \n' + \
 'Benchmark	Type	Metric	H/L	Acer Swift Go 14	Asus Zenbook 14	MSI Prestige 16	i7-1360P	R7-7840U	Apple M2	i7-1370P	Snapdragon X Elite	Lenovo X1 Carbon\n' + \
-'Sapphire Rapids - Multi-core	CPU	Score	H	1234			9283		810	\n'	 + \
+'Sapphirebench - Multi-core	CPU	Score	H	1234			9283		810	\n'	 + \
 'Cinebench R24 - Single Core	CPU	Score	H	12			1039		121		152 \n'	 + \
 'Grand Bench 6.2 - Multi-core	CPU	Score	H	123			111		10222		12223 \n' 
 
 
 instr_prompt_simple= 'Do the following instructions step by step.' + \
-'Step 1. Find all the processors mentioned in the document.' + \
-'Step 2. For each processor compare its performance numbers with other processors in the document\n' + \
-'Step 3. Get the performance number for each processor for each benchmark :\n' + \
-'Step 4. Generate a table as below for processors and their performance numbers.\n'
+'Step 1. Find all the laptops with specific processors mentioned.' + \
+'Step 2. Get the performance numbers for each laptop/processor for each benchmark :\n' + \
+'Step 3. For each laptop/processor compare its performance numbers with other laptops/processor in the document\n' + \
+'Step 4. Generate a table for each laptop/processor and their performance numbers compared to other laptops/processors.\n'
+
+#' The table columns should have these headers : Benchmark, Type, Metric, H/L and each laptop name. \n'
 
